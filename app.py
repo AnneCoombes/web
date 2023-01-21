9014086571abfd88107421d635cd5e0dd0c0c00d
Anne Coombes <anne@besabeau.com>
2023-01-21 12:03:44 +0000

    Initial Version

---
+++ app.py
@@ -0,0 +1,27 @@
+import web
+render = web.template.render('templates/')
+
+urls = (
+    '/health', 'health',
+    '/count', 'count',
+    '/(.*)', 'index'
+)
+
+class count:
+    def GET(self):
+        total = 0
+        i = web.input(num=1, start=1)
+        return render.count(int(i.num), int(i.start))
+
+
+class index:
+    def GET(self, name):
+        return render.index(name)
+
+class health:
+    def GET(self):
+        return "I am running"
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
